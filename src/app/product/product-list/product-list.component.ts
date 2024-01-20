b091026a05d8995276a2a20745a9612d2e129c6f
Jarek Wyprzal <jwyprzal@gmail.com>
2024-01-20 18:06:42 +0000

    add snackbar

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -9,11 +11,26 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((products) => {
       this.products = products;
+    });
+  }
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackBar.open('Product added to cart', 'Close', {
+          duration: 1000,
+          verticalPosition: 'top',
+          horizontalPosition: 'right',
+        });
+      },
     });
   }
 }
