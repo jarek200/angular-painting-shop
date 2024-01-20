4d3a7641510d022ee486402a54f8d76bc677128b
Jarek Wyprzal <jwyprzal@gmail.com>
2024-01-20 17:50:18 +0000

    add checkout api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    CartModule,
     BrowserModule,
     ProductModule,
     MatButtonModule,
--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
-  styleUrls: ['./cart-view.component.css']
+  styleUrls: ['./cart-view.component.css'],
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
+  cartItems: Product[] = [];
+  totalPrice: number = 0;
 
+  constructor(private cartService: CartService) {}
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe((data) => {
+      this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
+    });
+  }
+  getTotalPrice(): number {
+    return this.cartItems.reduce((acc, item) => acc + item.price, 0);
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart().subscribe();
+  }
+  checkout(): void {
+    this.cartService.checkout(this.cartItems).subscribe();
+  }
 }
