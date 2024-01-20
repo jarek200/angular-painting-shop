375eeee5e9834848c4781ef98895cc432b751b1f
Jarek Wyprzal <jwyprzal@gmail.com>
2024-01-20 18:37:59 +0000

    add sort to list component html

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -11,6 +11,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
+  sortOrder: string = '';
   constructor(
     private productService: ProductService,
     private cartService: CartService,
@@ -20,17 +22,35 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     this.productService.getProducts().subscribe((products) => {
       this.products = products;
+      this.filteredProducts = products;
     });
   }
   addToCart(product: Product): void {
     this.cartService.addToCart(product).subscribe({
       next: () => {
-        this.snackBar.open('Product added to cart', 'Close', {
+        this.snackBar.open('Product added to cart!', 'Close', {
           duration: 1000,
           verticalPosition: 'top',
           horizontalPosition: 'right',
         });
       },
     });
+  }
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+    this.filteredProducts = this.products.filter((product) =>
+      product.name.toLowerCase().includes(filterValue)
+    );
+    this.sortProducts(this.sortOrder);
+  }
+  sortProducts(sortValue: string): void {
+    this.sortOrder = sortValue;
+    if (this.sortOrder === 'priceLowHigh') {
+      this.filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (this.sortOrder === 'priceHighLow') {
+      this.filteredProducts.sort((a, b) => b.price - a.price);
+    }
   }
 }
